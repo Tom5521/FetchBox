4e57ad952d269d7ea5165c29fffc9c78a55fe53d
Tom5521 <mismarttv321@gmail.com>
2023-09-10 23:23:39 -0400

    Update the compile script

--- Build&Install.bat
+++ Build&Install.bat
@@ -1,13 +1,5 @@
 @echo off
-cd .\src\
-echo Compiling the golang binary...
-go build -o ..\builds\main.exe ..\main.go
-echo Go Binary Compiled!!!
-echo Packing python files with the golang binary...
-pyinstaller --onefile --distpath ..\builds --add-binary "../builds/main.exe;." --name InstallApps.exe main.py
-del src\InstallApps.exe.spec
-del src\build
-echo Packing Complete!!!
 
+go build -o "InstallApps.exe" main.go
 
 pause
