d3f228073a01e43caae7761b552dcce52fee13ff
Tom5521 <mismarttv321@gmail.com>
2023-10-20 15:35:18 -0300

    Move CLI mode to internal lib

---
+++ internal/cli/cli.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"FetchBox/cmd/core"
+	"fmt"
+	"os"
+)
+
+func Init() {
+	var (
+		install = core.Install{}
+	)
+	switch os.Args[1] {
+	case "scoop":
+		err := install.ScoopPkgInstall()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	case "choco":
+		err := install.ChocoPkgInstall()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	case "newyamlfile":
+		core.NewYamlFile() // Create a new yaml file
+	case "version":
+		fmt.Println(
+			"Windows-package-autoinstaller v"+core.Version,
+			"\nCreated by "+core.Yellow("Angel(Tom5521)")+"\nUnder the "+core.Red("MIT")+" License",
+		)
+	case "addCustomBuckets":
+		if len(os.Args) >= 3 {
+			err := core.ScoopBucketInstall(os.Args[2])
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -7,51 +7,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"FetchBox/cmd/core"
+	"FetchBox/internal/cli"
 	"FetchBox/internal/graph"
+	"os"
 )
 
 func main() {
 	if len(os.Args) > 1 {
 		if os.Args[1] != "dev" {
-			TermMode() // Check the cmd args
+			cli.Init() // Check the cmd args
 			return
 		}
 	}
 	graph.Init() // Initialize the graphical mode
-}
-
-func TermMode() {
-	var (
-		install = core.Install{}
-	)
-	switch os.Args[1] {
-	case "scoop":
-		err := install.ScoopPkgInstall()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	case "choco":
-		err := install.ChocoPkgInstall()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	case "newyamlfile":
-		core.NewYamlFile() // Create a new yaml file
-	case "version":
-		fmt.Println(
-			"Windows-package-autoinstaller v"+core.Version,
-			"\nCreated by "+core.Yellow("Angel(Tom5521)")+"\nUnder the "+core.Red("MIT")+" License",
-		)
-	case "addCustomBuckets":
-		if len(os.Args) >= 3 {
-			err := core.ScoopBucketInstall(os.Args[2])
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-		}
-	}
 }
